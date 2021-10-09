f9cbe2e81d5e302258636334eaab40d23a56a5f8
Max Santiago <mxxsant@hotmail.com>
2021-10-09 10:58:47 -0700

    feat: created pet-finder API fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { Client } from '@petfinder/petfinder-js';
+
+import { useEffect, useState } from 'react';
+
+const client = new Client({
+    apiKey: 'Uw2nyHWo7HuNa91CA030ycgBith6Bsiy85H0ldasaefSLpAstK',
+    secret: '44kOV61L3aNGRmZXIb4F87ZxCb3A7MzJbsGnc0aC',
+});
+
+function useFetch() {
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        client.animal
+            .search({ page: currentPage })
+            .then((response) => setData(response.data))
+            .catch((error) => console.log(error));
+    }, [currentPage]);
+
+    return [data, currentPage, setCurrentPage];
+}
+
+export default useFetch;
