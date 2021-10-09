49ac3bd6778c818b7a5174ebaafc88c3afd24feb
Max Santiago <mxxsant@hotmail.com>
2021-10-09 10:55:55 -0700

    fix: added missing extension operator to button component

--- src/components/atoms/button/Button.component.jsx
+++ src/components/atoms/button/Button.component.jsx
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 import { button } from './button.module.scss';
 
-const Button = ({ children }) => {
-    return <button className={button}>{children}</button>;
+const Button = ({ children, ...props }) => {
+    return (
+        <button className={button} {...props}>
+            {children}
+        </button>
+    );
 };
 
 Button.propTypes = {
