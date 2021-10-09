2f458abea35b659b0e189d02d4af02f3d6676093
Max Santiago <mxxsant@hotmail.com>
2021-10-09 11:04:12 -0700

    feat: added a new return when the API data has not been loaded

--- src/components/organisms/postsContainer/PostsContainer.component.jsx
+++ src/components/organisms/postsContainer/PostsContainer.component.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import Post from '../../molecules/Post/Post.component';
 import PostLive from '../../molecules/PostLive';
 
-import { postsContainer } from './posts-container.module.scss';
+import { postsContainer, chargingPosts } from './posts-container.module.scss';
 
-const PostsContainer = ({ data }) => {
+const PostsContainer = ({ posts }) => {
+    if (!posts) {
+        return (
+            <div className={chargingPosts}>
+                <h2>Charging posts...</h2>
+                <img src="/charging.gif" />
+            </div>
+        );
+    }
+
     return (
         <div className={postsContainer}>
             <div>
-                {data.map((post, index) => (
+                {posts.map((post, index) => (
                     <Post key={index} data={post} />
                 ))}
             </div>
-            <PostLive data={data[0]} />
+            <PostLive post={posts[0]} />
         </div>
     );
 };
