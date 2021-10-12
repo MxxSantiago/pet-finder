0b8968fb0b538199efeb111cc5cdd47b078309ba
Max Santiago <mxxsant@hotmail.com>
2021-10-12 15:49:07 -0700

    refactor: moved component logic to HomeTemplate component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import HomeTemplate from './components/templates/HomeTemplate.component';
-import useFetch from './hooks/useFetch';
 
 function App() {
-    const [data, currentPage, setCurrentPage] = useFetch();
-
     return (
         <div>
-            <HomeTemplate
-                data={data}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-            />
+            <HomeTemplate />
         </div>
     );
 }
