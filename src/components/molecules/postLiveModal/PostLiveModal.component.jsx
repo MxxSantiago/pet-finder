a6c7ff41754753e38f225db6f639f74b087aa251
Max Santiago <mxxsant@hotmail.com>
2021-10-12 15:52:54 -0700

    fix: adding wait animation correctly

--- src/components/molecules/postLiveModal/PostLiveModal.component.jsx
+++ src/components/molecules/postLiveModal/PostLiveModal.component.jsx
@@ -12,61 +12,62 @@ const PostLiveModal = ({ post, isOpen, handleClose, charging }) => {
     const { city, country } = post.contact.address;
     const postImage = post.photos[0] ? post.photos[0].medium : '/BannerBG.png';
 
-    if (!isOpen) return null;
-
-    if (charging) {
-        <div className="postLiveContainer">
-            <div className={styles.postLiveModal}>
-                <div className="chargingPost">
-                    <h2>Charging...</h2>
-                    <img alt={'charging'} src="/charging.gif" />
-                </div>
-            </div>
-        </div>;
-    }
-
     return ReactDOM.createPortal(
         <div className={styles.postLiveModalContainer}>
             <div className={styles.postLiveModal}>
-                <div className={styles.postLiveModalImageContainer}>
-                    <div className={styles.postLiveModalCloseBtn}>
-                        <Button onClick={() => handleClose()} children={'x'} />
+                {charging ? (
+                    <div className={styles.postLive}>
+                        <div className="chargingPost">
+                            <h2>Charging...</h2>
+                            <img alt={'charging'} src="/charging.gif" />
+                        </div>
                     </div>
-                    <img
-                        alt={name}
-                        draggable="false"
-                        className={styles.postLiveModalImage}
-                        src={postImage}
-                    />
-                </div>
-                <div className={styles.postLiveModalInfo}>
-                    <h1 className={styles.postLiveModalInfoName}>
-                        {name} ({type})
-                    </h1>
-                    <h1 className={styles.postLiveModalInfoType}></h1>
-                    <p className={styles.postLiveModalInfoAddress}>
-                        Location: {city} - {country}
-                    </p>
-                    <p className={styles.postLiveModalInfoDescription}>
-                        {description}
-                    </p>
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        className={styles.postLiveLink}
-                        href={url}
-                    >
-                        Complete information here
-                    </a>
-                    <div className={styles.postLiveModalInfoFooter}>
-                        <p className={styles.postLiveModalInfoMore}>
-                            {gender} - {age}
-                        </p>
-                        <p className={styles.postLiveModalInfoStatus}>
-                            {status}
-                        </p>
-                    </div>
-                </div>
+                ) : (
+                    <>
+                        <div className={styles.postLiveModalImageContainer}>
+                            <div className={styles.postLiveModalCloseBtn}>
+                                <Button
+                                    onClick={() => handleClose()}
+                                    children={'x'}
+                                />
+                            </div>
+                            <img
+                                alt={name}
+                                draggable="false"
+                                className={styles.postLiveModalImage}
+                                src={postImage}
+                            />
+                        </div>
+                        <div className={styles.postLiveModalInfo}>
+                            <h1 className={styles.postLiveModalInfoName}>
+                                {name} ({type})
+                            </h1>
+                            <h1 className={styles.postLiveModalInfoType}></h1>
+                            <p className={styles.postLiveModalInfoAddress}>
+                                Location: {city} - {country}
+                            </p>
+                            <p className={styles.postLiveModalInfoDescription}>
+                                {description}
+                            </p>
+                            <a
+                                target="_blank"
+                                rel="noreferrer"
+                                className={styles.postLiveLink}
+                                href={url}
+                            >
+                                Complete information here
+                            </a>
+                            <div className={styles.postLiveModalInfoFooter}>
+                                <p className={styles.postLiveModalInfoMore}>
+                                    {gender} - {age}
+                                </p>
+                                <p className={styles.postLiveModalInfoStatus}>
+                                    {status}
+                                </p>
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
         </div>,
         document.getElementById('modal')
