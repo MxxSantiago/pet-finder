9259e0e4199b7aba2a80bdd98779166c34580064
Max Santiago <mxxsant@hotmail.com>
2021-10-17 16:53:49 -0700

    feat: adding the logic to the searchBar component

--- src/components/molecules/SearchBar/SearchBar.component.jsx
+++ src/components/molecules/SearchBar/SearchBar.component.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Button from '../../atoms/button/Button.component';
 
 import { searchBar } from './search-bar.module.scss';
 
-const SearchBar = () => {
+const SearchBar = ({ setType }) => {
+    const inputRef = useRef();
+
+    const filterPerType = (event) => {
+        event.preventDefault();
+
+        const type = inputRef.current.value;
+        if (type.trim().length) {
+            setType(type);
+        } else {
+            alert('Enter a valid value...');
+        }
+
+        inputRef.current.value = '';
+    };
+
     return (
-        <div className={searchBar}>
-            <input type="text" placeholder="Enter pet parameters" />
-        </div>
+        <form className={searchBar} onSubmit={filterPerType}>
+            <input
+                type="text"
+                placeholder="Enter animal type"
+                ref={inputRef}
+                required={true}
+            />
+            <Button type="submit" children="search" />
+        </form>
     );
 };
 
