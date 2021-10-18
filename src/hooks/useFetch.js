890c6feff4d5dc5b8eb757261bb0dfda1d5ccc2d
Max Santiago <mxxsant@hotmail.com>
2021-10-17 17:00:15 -0700

    feat: adding a new property to call a specific segment of the API

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,17 +8,18 @@ const client = new Client({
 });
 
 function useFetch() {
+    const [type, setType] = useState('');
     const [data, setData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         client.animal
-            .search({ page: currentPage, limit: 10 })
+            .search({ page: currentPage, limit: 10, type: type })
             .then((response) => setData(response.data))
             .catch((error) => console.log(error));
-    }, [currentPage]);
+    }, [currentPage, type]);
 
-    return [data, currentPage, setCurrentPage];
+    return [data, currentPage, setCurrentPage, setType];
 }
 
 export default useFetch;
